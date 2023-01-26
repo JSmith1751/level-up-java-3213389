aaea912ff5fcdadff63234768ef7e4707fca0b1b
JSmith1751 <95705487+JSmith1751@users.noreply.github.com>
2023-01-26 17:21:23 +0000

    Implemented Person Class and Introduction

--- javacodechallenges/src/main/java/com/linkedin/javacodechallenges/App.java
+++ javacodechallenges/src/main/java/com/linkedin/javacodechallenges/App.java
@@ -1,9 +1,8 @@
 package com.linkedin.javacodechallenges;
 
-public class App 
-{
-    public static void main( String[] args )
-    {
-        
+public class App {
+    public static void main(String[] args) {
+        Person person1 = new Person("James", "Smith", 21);
+        person1.introduction();
     }
 }
