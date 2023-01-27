92aaaaa7430fa19271b9525f39a7e3cd049d4136
JSmith1751 <95705487+JSmith1751@users.noreply.github.com>
2023-01-27 15:57:52 +0000

    Implemented averageInvestment

--- javacodechallenges/src/main/java/com/linkedin/javacodechallenges/App.java
+++ javacodechallenges/src/main/java/com/linkedin/javacodechallenges/App.java
@@ -4,7 +4,11 @@ import java.util.List;
 
 public class App {
     public static double calculateAverageChangeInvested(List<Double> purchases) {
-        return 0;
+        Double invested = 0.0;
+        for (Double p : purchases) {
+            invested += Math.ceil(p) - p;
+        }
+        return purchases.size() == 0 ? 0 : invested / purchases.size();
     }
 
     public static void main(String[] args) {
