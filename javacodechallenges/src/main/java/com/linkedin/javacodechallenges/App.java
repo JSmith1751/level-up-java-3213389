0bd4b04963aea91e79f032d4715e4a04c3cc0fa3
JSmith1751 <95705487+JSmith1751@users.noreply.github.com>
2023-01-27 14:53:26 +0000

    Completed hundredDayAhead

--- javacodechallenges/src/main/java/com/linkedin/javacodechallenges/App.java
+++ javacodechallenges/src/main/java/com/linkedin/javacodechallenges/App.java
@@ -1,13 +1,24 @@
 package com.linkedin.javacodechallenges;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
 public class App 
 {
     // Create function to calculate the date that's 
     // 100 days from now
+    public static String hundredDaysAhead(){
+        Calendar c = Calendar.getInstance();
+        c.setTime(new Date());
+        c.add(Calendar.DATE, 100);
+        return c.getTime().toString();  
+    }
 
     public static void main( String[] args )
     {
         System.out.println("100 days from now is... " 
-            /* add function call */);
+            + hundredDaysAhead());
     }
 }
